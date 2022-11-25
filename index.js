80cbaa3e50eaebd15e078e14122dc4e0f1c6cc59
WesleyMatthee <wrmatthee@gmail.com>
2022-11-25 18:56:37 +0000

    Converting code to OOP. I cleaner way to work that makes reading code easier for all

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
+    logTaskState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    completeTask: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// prints out the provided task's details
+
+// marks the provided task as completed
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+// for now, let's just make sure we see our tasks
+//console.log(tasks);
+
+/*
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskTitles.push(description);
   taskComplete.push(false);
 }
 
@@ -29,3 +61,4 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
